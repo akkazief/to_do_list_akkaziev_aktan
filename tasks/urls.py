57725a86c9277c61196c3d36fcb396dc8df5620e
akkaziev_aktan <salizardfnite@gmail.com>
2026-05-25 16:50:30 +0600

    made front improvements added directory fixtures and dumped data

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,5 @@ from tasks.views import tasks, create_task, delete_task
 urlpatterns = [
     path('', tasks, name='index'),
     path('create_task/', create_task, name='create_task'),
-    path('tasks/delete_task/<int:pk>/', delete_task, name='delete_task'),
+    path('tasks.json/delete_task/<int:pk>/', delete_task, name='delete_task'),
 ]
