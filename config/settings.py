2845427744f39080ac4464eb84d714904b30dbc7
akkaziev_aktan <salizardfnite@gmail.com>
2026-05-28 16:38:55 +0600

    added psycopg2-binary and added to requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tasks_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
