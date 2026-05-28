080b1e23341f5b39b6e6ebf90f3bb5e2470f3b89
akkaziev_aktan <salizardfnite@gmail.com>
2026-05-28 17:46:24 +0600

    made base.html and directory tasks in templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tasks.views import tasks, create_task, delete_task
+from tasks.views import tasks, create_task, delete_task, details
 
 urlpatterns = [
     path('', tasks, name='main'),
     path('create_task/', create_task, name='create_task'),
-    path('tasks.json/delete_task/<int:pk>/', delete_task, name='delete_task'),
+    path('tasks/delete_task/<int:pk>/', delete_task, name='delete_task'),
+    path('tasks/<int:pk>/', details, name='details'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect,get_object_or_404, reverse
 
 from tasks.models import Task
 from tasks.forms import TaskForm
@@ -7,19 +7,24 @@ from tasks.forms import TaskForm
 def tasks(request):
     tasks = Task.objects.all()
     context = {'tasks': tasks}
-    return render(request, 'tasks.html', context)
+    return render(request, 'tasks/tasks.html', context)
 
 def create_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('main')
     else:
         form = TaskForm()
-    return render(request, 'create_task.html', {'form': form})
+    return render(request, 'tasks/create_task.html', {'form': form})
 
-def delete_task(pk):
+def delete_task(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
-    return redirect('/')
+    return redirect('main')
+
+def details(request, *args, pk, **kwargs):
+    task = get_object_or_404(Task, pk=pk)
+    context = {'task': task}
+    return render(request, "tasks/details.html", context)
