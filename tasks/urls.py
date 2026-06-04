bbf882db2bbb67bd55298aae2adf06188016803a
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-04 15:22:36 +0600

    deleted old delete_task url

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,6 @@ from tasks.views import tasks, create_task, delete_task, details, update_task
 urlpatterns = [
     path('', tasks, name='main'),
     path('create_task/', create_task, name='create_task'),
-    path('tasks/delete_task/<int:pk>/', delete_task, name='delete_task'),
     path('tasks/<int:pk>/', details, name='details'),
     path('tasks/<int:pk>/update', update_task, name='update_task'),
     path('tasks/<int:pk>/delete', delete_task, name='delete_task'),
